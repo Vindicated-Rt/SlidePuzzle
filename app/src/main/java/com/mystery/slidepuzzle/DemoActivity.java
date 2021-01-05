d35518611d15091c719fa94f65c8b22fc4d184a6
Vindicated-Rt <dengwenhao1105@gmail.com>
2021-01-05 16:36:19 +0800

    init DemoActivity

--- app/src/main/java/com/mystery/slidepuzzle/DemoActivity.java
+++ app/src/main/java/com/mystery/slidepuzzle/DemoActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DemoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
